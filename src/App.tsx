678d7cf306f28ef369c62eac865a404699ee9bf0
Catherine Plisson <catwtac@gmail.com>
2024-07-11 15:19:58 +0200

    maj 2 brevo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
@@ -8,10 +8,15 @@ import Tasks from './pages/Tasks';
 import About from './pages/About';
 import Taskrow from './components/TaskRow';
 import TaskForm from './components/TaskForm';
+import { initBrevoTracker } from './lib/trackerbrevo';
+
 
 
 const App: React.FC = () => {
   const [title, setTitle] = useState('');
+  useEffect(() => {
+    initBrevoTracker();
+  }, []);
   return (
     <Router>
       <div className="templateRow">
